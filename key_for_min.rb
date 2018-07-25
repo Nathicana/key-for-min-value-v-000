60350c2552a0493a4dafd08e175e932c05b2c40f
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-07-25 03:04:11 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -23,7 +23,19 @@ end
 
 
   def key_for_min_value(name_hash)
-    
-    
+    smallest_value = value
+     name_hash.each do |key, value|
+      # If current value is lower than default, change value&key 
+      if i[1] < d_value
+        d_value = i[1]
+        d_key = i[0]
+      end
+
+   end
+
+  return d_key
+
+end  
+
 
   
